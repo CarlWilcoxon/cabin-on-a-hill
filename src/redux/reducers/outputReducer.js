dc63246840d044a73442374fe9edce2745a64733
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-08-07 11:54:22 -0500

    Added some nuance to the output reducer.

--- src/redux/reducers/outputReducer.js
+++ src/redux/reducers/outputReducer.js
@@ -1,14 +1,14 @@
-const output = (state = ['taco', 'burrito'], action) => {
+const output = (state = ['You wake up on the floor of your bedroom. Morning light pours in through the BLINDS.'], action) => {
   // Use a switch statement to account for synonyms
   switch (action.type) {
     case 'STAND':
-      return ['You are now standing'];
+      return (state[0] === 'You are now standing.' || state[0] === 'You are still standing.') ? ['You are still standing.'] : ['You are now standing.'];
     case 'RISE':
-      return ['You are now standing'];
+      return (state[0] === 'You are now standing.' || state[0] === 'You are still standing.') ? ['You are still standing.'] : ['You are now standing.'];
     case 'KNEEL':
-      return ['You are now kneeling']
+      return (state[0] === 'You are now kneeling.' || state[0] === 'You are still kneeling.') ? ['You are still kneeling.'] : ['You are now kneeling.'];
     case 'PRONE':
-      return ['You are now prone']
+      return (state[0] === 'You are now prone.' || state[0] === 'You are still prone.') ? ['You are still prone.'] : ['You are now prone.'];
     case 'OUTPUT':
       return [action.payload]
 
