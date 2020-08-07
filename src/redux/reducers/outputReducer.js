19158f0e5e44f52a624477f78ab12b9fec7d75b5
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-08-06 22:07:34 -0500

    present this version, double check db first

--- src/redux/reducers/outputReducer.js
+++ src/redux/reducers/outputReducer.js
@@ -5,16 +5,10 @@ const output = (state = ['taco', 'burrito'], action) => {
       return ['You are now standing'];
     case 'RISE':
       return ['You are now standing'];
-    case 'GET' && action.payload[1] === 'UP':
-      return ['You are now standing']
-    case 'GET' && action.payload[1] === 'DOWN':
-      return ['You are now prone']
     case 'KNEEL':
       return ['You are now kneeling']
-
     case 'PRONE':
       return ['You are now prone']
-
     case 'OUTPUT':
       return [action.payload]
 
