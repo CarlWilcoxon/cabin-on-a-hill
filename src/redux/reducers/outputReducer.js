f11284dfa69e90f11767905de5e76ceb2df0a1a7
CarlWilcoxon <62306383+CarlWilcoxon@users.noreply.github.com>
2020-08-07 12:05:01 -0500

    cleaning up code

--- src/redux/reducers/outputReducer.js
+++ src/redux/reducers/outputReducer.js
@@ -5,20 +5,10 @@ const output = (state = ['You wake up on the floor of your bedroom. Morning ligh
       return ['You are now standing'];
     case 'RISE':
       return ['You are now standing'];
-    // case 'GET':
-    //   // Use action.payload for all words entered, that way I can check the
-    //   // second, third, etc words to find required combinations
-    //   switch (action.payload[1]) {
-    //     case 'UP':
-    //       return ['You are now standing']
-    //     case 'DOWN':
-    //       return ['You are now prone']
-    //   }
     case 'KNEEL':
-      return ['You are now kneeling']
-
+      return ['You are now kneeling'];
     case 'PRONE':
-      return ['You are now prone']
+      return ['You are now prone'];
 
     default:
       return state;
